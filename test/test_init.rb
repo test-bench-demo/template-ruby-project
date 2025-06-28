44669127127e523c741bf8a4fbbf7c512262625c
Nathan Ladd <nathanladd@gmail.com>
2025-06-28 11:30:01 -0400

    Initial commit

---
+++ test/test_init.rb
@@ -0,0 +1,13 @@
+ENV['CONSOLE_DEVICE'] ||= 'stdout'
+ENV['LOG_LEVEL'] ||= '_min'
+
+ENV['TEST_FIXTURE_DETAIL'] ||= ENV['D']
+
+puts RUBY_DESCRIPTION
+
+require_relative '../init'
+require 'TEMPLATE-PATH/controls'
+
+require 'TEMPLATE-TEST-BENCH-GEM-NAME'; TEMPLATE-TEST-BENCH-NAMESPACE.activate
+
+#TestBench::ImportConstants.(TEMPLATE-NAMESPACE)
